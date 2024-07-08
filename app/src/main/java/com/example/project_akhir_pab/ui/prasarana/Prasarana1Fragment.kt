d492bce76694bed14b1984f6b7aab113c340fb80
Abe <abraham120604@gmail.com>
2024-07-08 19:36:13 +0700

    updated judul prasarana

--- app/src/main/java/com/example/project_akhir_pab/ui/prasarana/Prasarana1Fragment.kt
+++ app/src/main/java/com/example/project_akhir_pab/ui/prasarana/Prasarana1Fragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project_akhir_pab.databinding.FragmentPrasarana1Binding
@@ -36,6 +37,7 @@ class Prasarana1Fragment : Fragment(), Prasarana1Adapter.OnItemClickCallback {
         binding.rvData1.layoutManager = LinearLayoutManager(requireContext())
         prasaranaAdapter = Prasarana1Adapter(prasaranaList)
         binding.rvData1.adapter = prasaranaAdapter
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Prasarana dan Prasarana Lain"
 
         fetchPrasaranaData()
     }
